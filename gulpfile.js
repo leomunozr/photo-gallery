d076641dae55697a0c14f115ce3422e44e4368d9
Leo <leonardomr1@gmail.com>
2019-03-06 17:25:33 -0600

    Update gulpfile to ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,35 @@
-var babelify = require('babelify'),
-  browserify = require("browserify"),
-  connect = require("gulp-connect"),
-  fs = require('fs'),
-  glob = require('glob'),
-  gulp = require('gulp'),
-  imageResize = require('gulp-image-resize');
-  path = require('path'),
-  rename = require("gulp-rename"),
-  source = require("vinyl-source-stream"),
-  buffer = require('vinyl-buffer'),
-  uglify = require('gulp-uglify'),
-  concat = require('gulp-concat'),
-  fonts = 'node_modules/bootstrap/fonts/*',
-  vendors = [
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/angular/angular.min.js',
-    'node_modules/angular-animate/angular-animate.min.js',
-    'node_modules/angular-route/angular-route.js',
-    'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
-    'node_modules/angulargrid/angulargrid.js'];
+const babelify = require('babelify');
+const browserify = require("browserify");
+const buffer = require('vinyl-buffer');
+const concat = require('gulp-concat');
+const connect = require("gulp-connect");
+const fs = require('fs');
+const glob = require('glob');
+const gulp = require('gulp');
+const path = require('path');
+const source = require("vinyl-source-stream");
+const uglify = require('gulp-uglify');
 
-var jsSources = glob.sync('./!(build|node_modules|bower_components)/*.js');
+const fonts = 'node_modules/bootstrap/fonts/*';
+const vendors = [
+  'node_modules/bootstrap/dist/js/bootstrap.min.js',
+  'node_modules/angular/angular.min.js',
+  'node_modules/angular-animate/angular-animate.min.js',
+  'node_modules/angular-route/angular-route.js',
+  'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
+  'node_modules/angulargrid/angulargrid.js'
+];
+const jsSources = glob.sync('./!(build|node_modules|bower_components)/*.js');
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const BUILD_DIR_JS = path.resolve(BUILD_DIR, 'js');
+const BUILD_DIR_CSS = path.resolve(BUILD_DIR, 'css');
+const BUILD_DIR_FONTS = path.resolve(BUILD_DIR, 'fonts');
 
 gulp.task('build', ['build:app', 'build:vendor', 'build:html', 'build:css']);
 
-gulp.task('build:app', function() {
-  console.log('Building...');
-  console.log(jsSources) 
+gulp.task('build:app', () => {
+  console.info('Building from: ', jsSources);
+
   return browserify({
     entries: jsSources
   })
@@ -35,87 +38,90 @@ gulp.task('build:app', function() {
     }))
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest(BUILD_DIR_JS));
 });
 
 gulp.task('build:vendor', () => {
+  console.info('Building vendor sources...');
+
   return browserify({ entries: vendors })
     .bundle()
     .pipe(source('vendor.js'))
     .pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest(BUILD_DIR_JS));
 });
 
 gulp.task('build:html', () => {
+  console.info('Copying HTML files to build dir...');
+
   gulp.src([
     '**/*.html',
     '!node_modules/**',
     '!build/**',
   ])
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest(BUILD_DIR));
 });
 
-gulp.task('build:css', function () {
+gulp.task('build:css', () => {
+  console.info('Copying CSS files to build dir...');
+
   gulp.src([
     'node_modules/bootstrap/dist/css/bootstrap.min.css',
     './css/styles.css'
   ])
     .pipe(concat('styles.css'))
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest(BUILD_DIR_CSS));
 });
 
-gulp.task('build:fonts', function() {
+gulp.task('build:fonts', () => {
+  console.info('Copying font files to build dir...');
+
   gulp.src(fonts)
-    .pipe(gulp.dest('./build/fonts'));
+    .pipe(gulp.dest(BUILD_DIR_FONTS));
 });
 
-gulp.task('listImages', function() {
-  console.log("Listing files...");
+gulp.task('listImages', () => {
+  console.info('Generating list of images...');
 
-  const basePath = 'images';
-  var images = [];
+  const basePath = path.resolve(__dirname, 'images');
 
-  var list_files = function(dir) {
-    var files = fs.readdirSync(dir);
-
-    files.forEach(file => {
+  function listFiles(dir) {
+    const files = fs.readdirSync(dir);
+    
+    return files.map((file) => {
       // No listar las imágenes minificadas
       if (file.includes('.min')) return;
 
-      var filePath = path.join(dir, file);
-      var stats = fs.statSync(filePath);
-
-      if (stats.isDirectory()) {
-        console.log(file + ' directory found.');
-        list_files(filePath);
+      const filePath = path.join(dir, file);
+      const fileStats = fs.statSync(filePath);
+      
+      if (fileStats.isDirectory()) {
+        console.info(`Found directory in ${file}.`);
+        return listFiles(filePath);
       }
 
-      else {
-        var image = {
-          'id': file,
-          'name': file,
-          'category': path.dirname(path.relative(basePath, filePath)),
-          'src': path.relative('.', filePath),
-          'min': path.relative('.', filePath).replace('.jpg', '.min.jpg')
-        }
-        images.push(image);
+      else return {
+        'id': file,
+        'name': file,
+        'category': path.dirname(path.relative(basePath, filePath)),
+        'src': path.relative('.', filePath),
+        'min': path.relative('.', filePath).replace('.jpg', '.min.jpg')
       }
-    });
+    })
+    .reduce((arr, img) => arr.concat(img), []);
+  };
 
-    return images;
-  }
+  const imagesList = listFiles(basePath);
 
-  var list = list_files(basePath);
-
-  images.sort(function(a, b) {
+  imagesList.sort((a, b) => {
     if (a.id > b.id) return 1;
     if (a.id < b.id) return -1;
     return 0;
   });
-  console.log(list);
 
-  fs.writeFileSync('js/images.json', JSON.stringify(images, null, 4));
+  fs.writeFileSync('js/images.json', JSON.stringify(imagesList, null, 2));
+  console.info('List of images generated successfully.');
 });
 
 gulp.task('minify', function() {
@@ -125,13 +131,13 @@ gulp.task('minify', function() {
     .pipe(gulp.dest('images'));
 });
 
-gulp.task('serve', function() {
-  connect.server({ root: 'build' });
+gulp.task('serve', () => {
+  connect.server({ root: BUILD_DIR });
 });
 
 gulp.task('default', ['listImages', 'minify', 'build', 'watch', 'serve']);
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch(jsSources, ['build']);
   gulp.watch('images/**', ['listImages', 'build']);
 });
